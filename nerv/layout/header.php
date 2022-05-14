61e71b4ba9b2d94f2aa1dbb24f5c77ba3117a2c4
kiyo <kosmoflips@gmail.com>
2022-05-14 17:36:08 -0600

    merge layout/header.php into layout/site_master1.php

--- nerv/layout/header.php
+++
@@ -1,3 +0,0 @@
-<div id="header-img"></div>
-<a href="/"><span id="headerlink"></span></a>
-<h1><a href="/"><?php echo POC_META['title'] ?></a></h1>
--- nerv/layout/site_master1.php
+++ nerv/layout/site_master1.php
@@ -6,7 +6,9 @@
 <body>
 <div id="master-wrap">
 <header id="header-outer">
-<?php include(POC_LAYOUT.'/header.php'); ?>
+<div id="header-img"></div>
+<a href="/"><span id="headerlink"></span></a>
+<h1><a href="/"><?php echo POC_META['title'] ?></a></h1>
 <?php include(POC_LAYOUT.'/menu.php'); ?>
 </header>
 <div id="content-wrap">
