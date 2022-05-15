51f8aaafb858de3538daa1508a9dc8977f26281f
kiyo <kosmoflips@gmail.com>
2022-05-14 19:52:49 -0600

    remove /idx and unused index modules

--- idx/themeinfo.php
+++
@@ -1,7 +0,0 @@
-<h4><?php echo rand_deco_symbol() ?> Theme</h4>
-<ul>
-<li>motif: <b>il mare dei suoni</b></li>
-<li>finish: Jan 24-25, 2020</li>
-<li><a href="https://www.jakpost.travel/vwall/obxTwx_deep-blue-sea-wallpaper-19201080-blue-sea-wallpapers/">Body background</a></li>
-<li><a href="https://www.hiclipart.com/free-transparent-background-png-clipart-qcyxq">Post background</a></li>
-</ul>
