0705401e33db44b644d009e1155db8f464807596
kiyo <kosmoflips@gmail.com>
2022-05-18 19:52:17 -0600

    add version info to footer.php

--- copy2drive.bat
+++ copy2drive.bat
@@ -5,6 +5,6 @@ REM run this script to make an immediate file copy to drive -- good for unfinali
 REM local dir for github development & for localhost testing
 SET localdir=D:\kiyoland\localhost\pocchong
 REM drive dir is for back up, may not be stable, files only, NOT version controlled
-SET drivedir=D:\Users\kosmo\OneDrive\pocchong
+SET drivedir=D:\Users\kosmo\OneDrive\gitrepo\pocchong
 
-robocopy %localdir% %drivedir% /e /v /mir /xd .git /xf .git .gitignore *.bat error.log readme.md desktop.ini
+robocopy %localdir% %drivedir% /e /v /mir /xd .git /xf .git .gitmodules .gitignore *.bat error.log readme.md desktop.ini
