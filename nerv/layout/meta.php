1fa849edeff9a0be4cfc9e1f2323dd21e6a04d81
kiyo <kosmoflips@gmail.com>
2022-10-28 23:40:12 -0600

    fix lightbox path

--- nerv/layout/meta.php
+++ nerv/layout/meta.php
@@ -2,9 +2,10 @@
 <!-- google fonts -->
 <link href="https://fonts.googleapis.com/css?family=Milonga&display=swap&subset=latin-ext" rel="stylesheet" /><!-- font-family: 'Milonga', cursive; -->
 <link href="https://fonts.googleapis.com/css2?family=Exo&display=swap" rel="stylesheet">
-<!-- other required -->
-<link rel="stylesheet" type="text/css" href="/dendron/deco/css/site.css" />
+<!-- javascript -->
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
 <script src="/dendron/deco/js/lightbox/lightbox.js"></script>
-<link rel="stylesheet" type="text/css" href="/deco/js/lightbox/lightbox.css" />
+<!-- css -->
+<link rel="stylesheet" type="text/css" href="/dendron/deco/js/lightbox/lightbox.css" />
+<link rel="stylesheet" type="text/css" href="/dendron/deco/css/site.css" />
 <?php $this->html_head_stuff(); ?>
