48982d1a59835955e24a9908e0c74f9d43857905
kiyo <kosmoflips@gmail.com>
2022-12-31 16:18:45 -0700

    minor edits

--- nerv/layout/meta.php
+++ nerv/layout/meta.php
@@ -1,7 +1,7 @@
 <title><?php $this->html_head_title() ?></title>
 <!-- google fonts -->
 <link href="https://fonts.googleapis.com/css?family=Milonga&display=swap&subset=latin-ext" rel="stylesheet" /><!-- font-family: 'Milonga', cursive; -->
-<link href="https://fonts.googleapis.com/css2?family=Exo&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Exo&display=swap" rel="stylesheet" />
 <!-- javascript -->
 <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
 <script src="/deco/js/lightbox/lightbox.js"></script>
